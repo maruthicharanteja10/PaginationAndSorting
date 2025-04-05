f35efeea83ed584a1152804f0f7214bbc605883a
maruthicharanteja <124443822+maruthicharanteja10@users.noreply.github.com>
2025-04-05 22:33:03 +0530

    Add files via upload

---
+++ PaginationWithEmployeeManagement/src/main/java/com/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.entity.Employee;
+
+
+
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+}
